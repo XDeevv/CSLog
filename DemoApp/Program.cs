5aa0fae799efc10bf8f9f598c2b82d1ff24cb461
XDev <88006874+XDeevv@users.noreply.github.com>
2023-03-24 21:10:31 +0200

    Add files via upload

---
+++ DemoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using CSLog;
+using CSLog.Parsing;
+using System;
+
+namespace DemoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CSLCustom pattern = new CSLCustom("<BG=DarkBlue><FG=Yellow>{Timestamp} <FG=Red>[CUSTOM] <FG=Gray>: <FG=Green>{Message}");
+
+			CSLogger.Log(Types.Debug, "Debug log");
+			CSLogger.Log(Types.Info, "Info log");
+			CSLogger.Log(Types.Warning, "Warning log");
+			CSLogger.Log(Types.Error, "Error log");
+			CSLogger.Log(Types.Critical, "Critical log");
+            Console.WriteLine();
+            CSLogger.LogWithCustomProfile(pattern, "Custom log");
+		}
+    }
+}
