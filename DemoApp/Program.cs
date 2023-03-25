34d99ec811bc6714c596b7043355e8559fe64d3a
XDev <88006874+XDeevv@users.noreply.github.com>
2023-03-25 14:53:23 +0200

    Update Program.cs

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using CSLog;
+using CSLog;
 using CSLog.Parsing;
 using System;
 
@@ -8,7 +8,7 @@ namespace DemoApp
     {
         public static void Main(string[] args)
         {
-            CSLCustom pattern = new CSLCustom("<BG=DarkBlue><FG=Yellow>{Timestamp} <FG=Red>[CUSTOM] <FG=Gray>: <FG=Green>{Message}");
+            CSLCustom pattern = new CSLCustom("<FG=DarkGray>{Timestamp} <RS>[<FG={EX=3}>{EX=2}<RS>] | <BG={EX=1}><FG=Black>{EX=0}<RS> | : {Message}");
 
 			CSLogger.Log(Types.Debug, "Debug log");
 			CSLogger.Log(Types.Info, "Info log");
@@ -16,7 +16,7 @@ namespace DemoApp
 			CSLogger.Log(Types.Error, "Error log");
 			CSLogger.Log(Types.Critical, "Critical log");
             Console.WriteLine();
-            CSLogger.LogWithCustomProfile(pattern, "Custom log");
+            CSLogger.LogWithCustomProfile(pattern, "Custom log", "Server", "Blue", "Custom", "Magenta");
 		}
     }
 }
